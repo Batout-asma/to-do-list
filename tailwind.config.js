19a5cf83fcf87e7b3cd406e09b68fcdd4f4e6cf6
Batout-asma <benramoulasma@gmail.com>
2025-02-05 15:28:14 +0100

    glow color to green

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,12 @@ export default {
     extend: {
       keyframes: {
         glow: {
-          "0%, 100%": { textShadow: "0 0 8px rgba(255, 255, 255, 0.8)" },
-          "50%": { textShadow: "0 0 16px rgba(255, 255, 255, 1)" },
+          "0%, 100%": { textShadow: "0 0 8px rgba(34, 197, 94, 1)" },
+          "50%": { textShadow: "0 0 16px rgba(255, 255, 255, 0.5)" },
         },
       },
       animation: {
-        glow: "glow 1.5s ease-in-out infinite",
+        glow: "glow 2.5s ease-in-out infinite",
       },
     },
   },
